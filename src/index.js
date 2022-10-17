0894f78db6698f155ccb353c0768810215a60fb9
Miquel Rossello <mkpuig@gmail.com>
2022-10-17 11:42:46 +0200

    fix imports

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
-const express = require('express')
-const asyncHandler = require('express-async-handler')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { buildGetPaymentDestinationRouter } = require('./buildGetPaymentDestinationRouter')
-const { buildIdentityRouter } = require('./buildIndentityRouter')
-const { buildVerifyPubkeyRouter } = require('./buildVerifyPubkeyRouter')
-const { buildPublicProfileRouter } = require('./buildPublicProfileRouter')
-const { buildAssetInformationRouter } = require('./buildAssetInformationRouter')
-const { buildP2pPaymentDestinationRouter } = require('./buildP2pPaymentDestinationRouter')
-const { buildP2pPaymentDestinationTokenRouter } = require('./buildP2pPaymentDestinationTokensRouter')
-const { errorHandler } = require('./error-handler')
-const { PaymailClient } = require('paymail-client')
-const dns = require('dns')
-const fetch = require('isomorphic-fetch')
-const urljoin = require('url-join')
-const { URL } = require('url')
-const { CapabilityCodes } = require('./constants')
-const { buildReceiveTransactionRouter } = require('./buildReceiveTransactionRouter')
-const { PaymailError } = require('./errors/PaymailError')
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { buildGetPaymentDestinationRouter } = require("./buildGetPaymentDestinationRouter");
+const { buildIdentityRouter } = require("./buildIndentityRouter");
+const { buildVerifyPubkeyRouter } = require("./buildVerifyPubkeyRouter");
+const { buildPublicProfileRouter } = require("./buildPublicProfileRouter");
+const { buildAssetInformationRouter } = require("./buildAssetInformationRouter");
+const { buildP2pPaymentDestinationRouter } = require("./buildP2pPaymentDestinationRouter");
+const { buildP2pPaymentDestinationTokenRouter } = require("./buildP2pPaymentDestinationTokensRouter");
+const { errorHandler } = require("./error-handler");
+const { PaymailClient } = require("paymail-client");
+const dns = require("dns");
+const fetch = require("node-fetch");
+const urljoin = require("url-join");
+const { URL } = require("url");
+const { CapabilityCodes } = require("./constants");
+const { buildReceiveTransactionRouter } = require("./buildReceiveTransactionRouter");
+const { PaymailError } = require("./errors/PaymailError");
 
 const getBaseRoute = (config) => {
-  return config.basePath || '/'
-}
+  return config.basePath || "/";
+};
 
 const joinUrls = (...parts) => {
-  return urljoin(...parts)
-}
+  return urljoin(...parts);
+};
 
 const validateBaseUrl = (url) => {
   try {
-    url = new URL(url)
-    if (url.protocol !== 'https:' && /^locahost:?\d*$/.test(url.hostname)) {
-      console.warn('Paymail should always use ssl on production')
+    url = new URL(url);
+    if (url.protocol !== "https:" && /^locahost:?\d*$/.test(url.hostname)) {
+      console.warn("Paymail should always use ssl on production");
     }
-    return url
+    return url;
   } catch (err) {
-    throw new Error(`Invalid base url: ${url}`, err)
+    throw new Error(`Invalid base url: ${url}`, err);
   }
-}
+};
 
 /**
  * @callback getIdentityKey
@@ -70,87 +70,112 @@ const validateBaseUrl = (url) => {
  */
 
 const buildPaymailRouter = (baseUrl, config) => {
-  const baseRouter = express.Router()
-  baseRouter.use(bodyParser.json({ ...config.bodyParserConfig, type: 'application/json' }))
-  const apiRouter = express.Router()
-  baseUrl = validateBaseUrl(baseUrl)
+  const baseRouter = express.Router();
+  baseRouter.use(bodyParser.json({ ...config.bodyParserConfig, type: "application/json" }));
+  const apiRouter = express.Router();
+  baseUrl = validateBaseUrl(baseUrl);
 
   if (config.useCors) {
-    baseRouter.use(cors(config.corsConfig || {}))
+    baseRouter.use(cors(config.corsConfig || {}));
   }
 
-  const capabilities = config.capabilities || {}
+  const capabilities = config.capabilities || {};
 
-  capabilities[CapabilityCodes.requestSenderValidation] = !!config.requestSenderValidation
+  capabilities[CapabilityCodes.requestSenderValidation] = !!config.requestSenderValidation;
 
   buildIdentityRouter(config, (router) => {
-    apiRouter.use(router)
-    capabilities.pki = joinUrls(baseUrl.href, getBaseRoute(config), '/id/{alias}@{domain.tld}')
-  })
+    apiRouter.use(router);
+    capabilities.pki = joinUrls(baseUrl.href, getBaseRoute(config), "/id/{alias}@{domain.tld}");
+  });
 
   buildGetPaymentDestinationRouter(config, (router) => {
-    apiRouter.use(router)
-    capabilities.paymentDestination = joinUrls(baseUrl.href, getBaseRoute(config), '/address/{alias}@{domain.tld}')
-  })
+    apiRouter.use(router);
+    capabilities.paymentDestination = joinUrls(baseUrl.href, getBaseRoute(config), "/address/{alias}@{domain.tld}");
+  });
 
   buildVerifyPubkeyRouter(config, (router) => {
-    apiRouter.use(router)
-    capabilities[CapabilityCodes.verifyPublicKeyOwner] = joinUrls(baseUrl.href, getBaseRoute(config), '/verifypubkey/{alias}@{domain.tld}/{pubkey}')
-  })
+    apiRouter.use(router);
+    capabilities[CapabilityCodes.verifyPublicKeyOwner] = joinUrls(
+      baseUrl.href,
+      getBaseRoute(config),
+      "/verifypubkey/{alias}@{domain.tld}/{pubkey}"
+    );
+  });
 
   buildPublicProfileRouter(config, (router) => {
-    console.warn('This feature is in alpha state. Paymail profile protocol is still being discussed.')
-    apiRouter.use(router)
-    capabilities[CapabilityCodes.publicProfile] = joinUrls(baseUrl.href, getBaseRoute(config), '/public-profile/{alias}@{domain.tld}')
-  })
+    console.warn("This feature is in alpha state. Paymail profile protocol is still being discussed.");
+    apiRouter.use(router);
+    capabilities[CapabilityCodes.publicProfile] = joinUrls(
+      baseUrl.href,
+      getBaseRoute(config),
+      "/public-profile/{alias}@{domain.tld}"
+    );
+  });
 
   buildReceiveTransactionRouter(config, (router) => {
     // console.warn('This feature is in alpha state. Paymail profile protocol is still being discussed.')
-    apiRouter.use(router)
-    capabilities[CapabilityCodes.receiveTransaction] = joinUrls(baseUrl.href, getBaseRoute(config), '/receive-transaction/{alias}@{domain.tld}')
-  })
+    apiRouter.use(router);
+    capabilities[CapabilityCodes.receiveTransaction] = joinUrls(
+      baseUrl.href,
+      getBaseRoute(config),
+      "/receive-transaction/{alias}@{domain.tld}"
+    );
+  });
 
   buildP2pPaymentDestinationRouter(config, (router) => {
-    apiRouter.use(router)
-    capabilities[CapabilityCodes.p2pPaymentDestination] = joinUrls(baseUrl.href, getBaseRoute(config), '/p2p-payment-destination/{alias}@{domain.tld}')
-  })
+    apiRouter.use(router);
+    capabilities[CapabilityCodes.p2pPaymentDestination] = joinUrls(
+      baseUrl.href,
+      getBaseRoute(config),
+      "/p2p-payment-destination/{alias}@{domain.tld}"
+    );
+  });
 
   buildP2pPaymentDestinationTokenRouter(config, (router) => {
-    apiRouter.use(router)
-    capabilities[CapabilityCodes.p2pPaymentDestinationWithTokensSupport] = joinUrls(baseUrl.href, getBaseRoute(config), '/p2p-payment-destination-tokens-support/{alias}@{domain.tld}')
-  })
+    apiRouter.use(router);
+    capabilities[CapabilityCodes.p2pPaymentDestinationWithTokensSupport] = joinUrls(
+      baseUrl.href,
+      getBaseRoute(config),
+      "/p2p-payment-destination-tokens-support/{alias}@{domain.tld}"
+    );
+  });
 
   buildAssetInformationRouter(config, (router) => {
-    apiRouter.use(router)
-    capabilities[CapabilityCodes.assetInformation] = joinUrls(baseUrl.href, getBaseRoute(config), '/asset/{alias}@{domain.tld}')
-  })
+    apiRouter.use(router);
+    capabilities[CapabilityCodes.assetInformation] = joinUrls(
+      baseUrl.href,
+      getBaseRoute(config),
+      "/asset/{alias}@{domain.tld}"
+    );
+  });
 
-  baseRouter.get('/.well-known/bsvalias', asyncHandler(async (req, res) => {
-    res.type('application/json')
-    res.send({
-      bsvalias: '1.0',
-      capabilities
+  baseRouter.get(
+    "/.well-known/bsvalias",
+    asyncHandler(async (req, res) => {
+      res.type("application/json");
+      res.send({
+        bsvalias: "1.0",
+        capabilities,
+      });
     })
-  }))
+  );
 
-  baseRouter.use(getBaseRoute(config), apiRouter)
+  baseRouter.use(getBaseRoute(config), apiRouter);
 
-  baseRouter.use(config.errorHandler)
+  baseRouter.use(config.errorHandler);
 
-  return baseRouter
-}
+  return baseRouter;
+};
 
 const buildRouter = (baseDomain, config) => {
-  return buildPaymailRouter(baseDomain,
-    {
-      paymailClient: new PaymailClient(dns, fetch),
-      useCors: true,
-      corsConfig: {},
-      errorHandler: errorHandler,
-      bodyParserConfig: {},
-      ...config
-    }
-  )
-}
+  return buildPaymailRouter(baseDomain, {
+    paymailClient: new PaymailClient(dns, fetch),
+    useCors: true,
+    corsConfig: {},
+    errorHandler: errorHandler,
+    bodyParserConfig: {},
+    ...config,
+  });
+};
 
-module.exports = { buildRouter, PaymailError }
+module.exports = { buildRouter, PaymailError };
